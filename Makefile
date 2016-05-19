03059c53963bb8b057c5750843dc272818f67c26
Victor Adam <victor@drawall.cc>
2016-05-19 11:44:34 +0200

    Generate .ogg files for a mod

--- Makefile
+++ Makefile
@@ -1,2 +1,47 @@
+CC = clang
+CFLAGS += -std=c99 -pedantic -march=native -fstrict-aliasing -fstrict-overflow
+CFLAGS += -Weverything -Werror -Wno-c++-compat
+# CFLAGS += -Ofast -fno-asynchronous-unwind-tables
+CFLAGS += -O1 -ggdb -fsanitize=address,leak,undefined
+CFLAGS += -D_XOPEN_SOURCE=500
+
 bpm: bpm.c
-	cc -lm bpm.c
+	$(CC) $(CFLAGS) -lm $< -o $@
+
+raw/%.raw: beatmaps/%.txt bpm
+	./bpm $< >$@
+
+ogg/%.ogg: raw/%.raw
+	sox -esigned-integer -Lb16 -r44100 $< $@
+ogg/%c.ogg: raw/%.raw
+	sox -esigned-integer -Lb16 -r44100 $< $@
+ogg/%h.ogg: raw/%.raw
+	sox -esigned-integer -Lb16 -r44100 $< $@
+
+SOUNDTRACK = _3
+.PHONY: all
+all: ogg/boss_1$(SOUNDTRACK).ogg
+all: ogg/boss_2$(SOUNDTRACK).ogg
+all: ogg/boss_3$(SOUNDTRACK).ogg
+all: ogg/boss_4$(SOUNDTRACK).ogg
+all: ogg/boss_5$(SOUNDTRACK).ogg
+all: ogg/boss_6$(SOUNDTRACK).ogg
+all: ogg/boss_7$(SOUNDTRACK).ogg
+all: ogg/boss_8$(SOUNDTRACK).ogg
+all: ogg/lobby$(SOUNDTRACK).ogg
+all: ogg/training$(SOUNDTRACK).ogg
+all: ogg/zone1_1$(SOUNDTRACK).ogg
+all: ogg/zone1_2$(SOUNDTRACK).ogg
+all: ogg/zone1_3$(SOUNDTRACK).ogg
+all: ogg/zone2_1$(SOUNDTRACK).ogg
+all: ogg/zone2_2$(SOUNDTRACK).ogg
+all: ogg/zone2_3$(SOUNDTRACK).ogg
+all: ogg/zone3_1$(SOUNDTRACK)c.ogg
+all: ogg/zone3_1$(SOUNDTRACK)h.ogg
+all: ogg/zone3_2$(SOUNDTRACK)c.ogg
+all: ogg/zone3_2$(SOUNDTRACK)h.ogg
+all: ogg/zone3_3$(SOUNDTRACK)c.ogg
+all: ogg/zone3_3$(SOUNDTRACK)h.ogg
+all: ogg/zone4_1$(SOUNDTRACK).ogg
+all: ogg/zone4_2$(SOUNDTRACK).ogg
+all: ogg/zone4_3$(SOUNDTRACK).ogg
--- bpm.c
+++ bpm.c
@@ -7,39 +7,55 @@
 #define MAX_VOL 0x8000
 
 // Returns the frequency of a note `t` half-tones above LA
-#define HZ(t) (LA * pow(2, (double) t / 12))
+#define HZ(ht) (LA * pow(2, (double) (ht) / 12))
+#define HALF_TONES(t) ((int[]) {0, 2, 3, 5, 7, 8, 10} [(t) % 7] + (t) / 7 * 12 )
 
 // Gives the `i`th sample for a note of frequency `hz`
-#define SAMPLE(i, hz) (sin((i) * (hz) * 2 * M_PI / SAMPLE_RATE))
+#define FRAC(x) ((x) - (long) (x))
+#define SAMPLE(i, hz) (fabs(2 - 4 * FRAC((i) * (hz) / SAMPLE_RATE)) - 1)
+// #define SAMPLE(i, hz) (sin(2 * M_PI * (i) * (hz) / SAMPLE_RATE))
 
 #define NORMALIZE(x, from, to) (((x) - (from)) / ((to) - (from)))
-#define EASE(x) (1 - (1 - (x)) * (1 - (x)))
+// #define EASE(x) (1 - (1 - x) * (1 - x) * (1 - x))
+#define EASE(x) ((1 - cos((x) * M_PI)) / 2)
 #define F(x, from, to) EASE(NORMALIZE(x, from, to))
 
-#define ATTACK  0.1
-#define DECAY   0.2
-#define RELEASE 0.9
-#define SUSTAIN 0.8
+#define ATTACK  .02
+#define DECAY   .18
+#define RELEASE .36
+#define SUSTAIN .9
 
 // major scale
 // int scale[7] = {0, 2, 4, 5, 7, 9, 11};
-// minor scale
-int scale[7] = {0, 2, 3, 5, 7, 8, 10};
+
+static long i;
+static long last_beat;
+static unsigned int beat;
+
+static void do_beat(long next_beat) {
+	beat++;
+	beat += next_beat > last_beat + 20000;
+	double hz = HZ(HALF_TONES(__builtin_ctz(beat)) - 8);
+	while (++i < next_beat) {
+		double x = (double) (i - last_beat) / (next_beat - last_beat);
+		double vol = x < ATTACK  ? F(x, 0, ATTACK) :
+					x < DECAY   ? 1 - (1 - SUSTAIN) * F(x, ATTACK, DECAY) :
+					x < RELEASE ? SUSTAIN * (1 - F(x, DECAY, RELEASE)) : 0;
+		int out = (int) (MAX_VOL * vol * SAMPLE(i, hz));
+		putchar(out & 255);
+		putchar(out >> 8);
+	}
+	last_beat = i;
+}
 
 int main(int argc, char **argv) {
-	FILE* aplay = popen("aplay -fS16_LE -r44100", "w");
-	long bpm = argc > 1 ? atoi(argv[1]) : 300;
-	long spb = SAMPLE_RATE * 60 / bpm;
-	long beat = 1;
+	// FILE* aplay = popen("aplay -fS16_LE -r44100", "w");
+	FILE* beatmap = fopen(argv[argc - 1], "r");
+	long next_beat = 0;
 
-	for (long i = 0; ++i;) {
-		beat += !(i % spb);
-		double hz = HZ(scale[__builtin_ctz(beat)]);
-		double x = (double) (i % spb) / spb;
-		double vol = x < ATTACK  ? F(x, 0, ATTACK) :
-					 x < DECAY   ? 1 - (1 - SUSTAIN) * F(x, ATTACK, DECAY) :
-					 x < RELEASE ? SUSTAIN * (1 - F(x, DECAY, RELEASE)) : 0;
-		int out = MAX_VOL * vol * SAMPLE(i, hz);
-		fwrite(&out, 2, 1, aplay);
+	while (fscanf(beatmap, "%ld,", &next_beat) > 0) {
+		next_beat = next_beat * SAMPLE_RATE / 1000;
+		do_beat((next_beat + last_beat) / 2);
+		do_beat(next_beat);
 	}
 }
